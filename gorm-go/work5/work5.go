c59608cff621f9d978dcbfae37a20d4637b39b2f
renny <rey888@126.com>
2025-06-21 21:55:32 +0800

    修正作业gorm进阶

--- gorm-go/work5/work5.go
+++ gorm-go/work5/work5.go
@@ -11,7 +11,7 @@ type (
 	User struct {
 		Id        uint `gorm:"primary_key;auto_increment;column:id" json:"id"`
 		Name      string
-		Posts     []Post `gorm:"foreignKey:id"`
+		Posts     []Post `gorm:"foreignKey:user_id"`
 		PostCount int    `gorm:"column:post_count"`
 	}
 
@@ -19,26 +19,30 @@ type (
 		Id           uint `gorm:"primary_key;auto_increment;column:id" json:"id"`
 		Title        string
 		Content      string
-		User         User      `gorm:"foreignKey:id"`
-		Comments     []Comment `gorm:"foreignKey:id"`
+		UserId       uint
+		User         User      `gorm:"foreignKey:user_id"`
+		Comments     []Comment `gorm:"foreignKey:post_id"`
 		CommentCount int       `gorm:"column:comment_count"`
 	}
 
 	Comment struct {
 		Id      uint `gorm:"primary_key;auto_increment;column:id" json:"id"`
 		Content string
-		Post    Post `gorm:"foreignKey:id"`
+		PostId  uint
+		Post    Post `gorm:foreignKey:post_id`
 	}
 )
 
 func (p *Post) AfterCreate(tx *gorm.DB) (err error) {
-	p.User.PostCount++
-	tx.Save(&p.User)
+	tx.Model(&User{}).Where("id = ?", p.UserId).Update("post_count", gorm.Expr("post_count + ?", 1))
+	// p.User.PostCount++
+	// tx.Save(&p.User)
 	return nil
 }
 func (c *Comment) AfterCreate(tx *gorm.DB) (err error) {
-	c.Post.CommentCount++
-	tx.Save(&c.Post)
+	tx.Model(&Post{}).Where("id = ?", c.PostId).Update("comment_count", gorm.Expr("comment_count + ?", 1))
+	// c.Post.CommentCount++
+	// tx.Save(&c.Post)
 	return nil
 }
 
@@ -51,22 +55,26 @@ func main() {
 	if err2 != nil {
 		panic(err2)
 	}
-	// db.Where("1=1").Delete(&User{})
-	// db.Where("1=1").Delete(&Post{})
-	// db.Where("1=1").Delete(&Comment{})
+	db.Where("1=1").Delete(&User{})
+	db.Where("1=1").Delete(&Post{})
+	db.Where("1=1").Delete(&Comment{})
 
-	// u := User{Name: "Jinzhu"}
-	// db.Create(&u)
-	// db.Create(&Post{Title: "Post 1", Content: "Content 1", User: u})
-	// db.Create(&Post{Title: "Post 2", Content: "Content 2", User: u})
-	// db.Create(&Post{Title: "Post 3", Content: "Content 3", User: u})
-	// db.Create(&Post{Title: "Post 4", Content: "Content 4", User: u})
+	u := User{Name: "Jinzhu"}
+	p1 := Post{Title: "Post 1", Content: "Content 1", User: u, UserId: u.Id}
+	p2 := Post{Title: "Post 2", Content: "Content 2", User: u, UserId: u.Id}
+	p3 := Post{Title: "Post 3", Content: "Content 3", User: u, UserId: u.Id}
+	p4 := Post{Title: "Post 4", Content: "Content 4", User: u, UserId: u.Id}
+	db.Create(&u)
+	db.Create(&p1)
+	db.Create(&p2)
+	db.Create(&p3)
+	db.Create(&p4)
 
-	// db.Create(&Comment{Content: "Comment 1", Post: Post{Id: 1}})
-	// db.Create(&Comment{Content: "Comment 2", Post: Post{Id: 2}})
-	// db.Create(&Comment{Content: "Comment 3", Post: Post{Id: 3}})
-	// db.Create(&Comment{Content: "Comment 4", Post: Post{Id: 4}})
-	// db.Create(&Comment{Content: "Comment 5", Post: Post{Id: 4}})
+	db.Create(&Comment{Content: "Comment 1", Post: p1, PostId: p1.Id})
+	db.Create(&Comment{Content: "Comment 2", Post: p2, PostId: p2.Id})
+	db.Create(&Comment{Content: "Comment 3", Post: p3, PostId: p3.Id})
+	db.Create(&Comment{Content: "Comment 4", Post: p4, PostId: p4.Id})
+	db.Create(&Comment{Content: "Comment 5", Post: p4, PostId: p4.Id})
 
 	var posts []Post
 	err1 := db.Preload("Comments").Where("1=1").Find(&posts)
